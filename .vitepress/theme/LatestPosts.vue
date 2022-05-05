84d854e2c7845d732951049ca09d2cd573123d3d
Jacob Long <jclong98@gmail.com>
2022-05-04 19:21:46 -0700

    attempted fix for home

--- .vitepress/theme/LatestPosts.vue
+++ .vitepress/theme/LatestPosts.vue
@@ -6,7 +6,9 @@ const latestPosts = posts.slice(0, 3)
 </script>
 <template>
   <div>
-    <ul class="grid sm:grid-cols-3 gap-4 mt-8 grow">
+    <h2 class="text-2xl mt-8 mb-4">Recent Posts</h2>
+
+    <ul class="grid sm:grid-cols-3 gap-4">
       <li
         v-for="{ title, href, date, excerpt } of latestPosts"
         class="border p-4"
@@ -26,6 +28,7 @@ const latestPosts = posts.slice(0, 3)
         </article>
       </li>
     </ul>
+
     <div class="mt-4 mb-8">
       <a href="/posts"> View All Blog Posts →</a>
     </div>
