6345818b0582ba759fca9f9057368ec3eda165a6
Jacob Long <jclong98@gmail.com>
2022-05-09 13:27:27 -0700

    add more opengraph things

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vitepress'
 
+const description = Jacob Long's personal website
+
 export default defineConfig({
   title: 'Jacob Long',
-  description: "Jacob Long's personal website",
+  description: description,
   head: [
-    ['meta', { name: 'twitter:site', content: '@jclong98' }],
-    [
-      'meta',
-      { name: 'twitter:card', content: 'https://jacoblong.dev/og_image.png' },
-    ],
-    [
-      'meta',
-      {
-        name: 'twitter:image',
-        content: 'https://jacoblong.dev/og_image.png',
-      },
-    ],
     [
       'link',
       {
@@ -28,6 +18,34 @@ export default defineConfig({
       'script',
       {
         src: '/setColorScheme.js',
+      },
+    ],
+
+    // og:
+    ['meta', { property: 'og:url', content: 'https://jacoblong.dev/' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:title', content: 'jacoblong.dev' }],
+    [
+      'meta',
+      { property: 'og:description', content: description },
+    ],
+    [
+      'meta',
+      { property: 'og:image', content: 'https://jacoblong.dev/og_image.png' },
+    ],
+
+    // twitter:
+    ['meta', { name: 'twitter:site', content: '@jclong98' }],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:domain', content: 'jacoblong.dev' }],
+    ['meta', { name: 'twitter:url', content: 'https://jacoblong.dev' }],
+    ['meta', { name: 'twitter:title', content: 'jacoblong.dev' }],
+    ['meta', { name: 'twitter:description', content: description }],
+    [
+      'meta',
+      {
+        name: 'twitter:image',
+        content: 'https://jacoblong.dev/og_image.png',
       },
     ],
   ],
