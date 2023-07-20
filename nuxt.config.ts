5ebeb243f0980edcd387f23a16249db70b89584c
Jacob Long <jclong98@gmail.com>
2023-07-20 10:47:32 -0700

    add nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/content", "@unocss/nuxt", "@vueuse/nuxt"],
+  modules: ["@nuxt/content", "@unocss/nuxt", "@vueuse/nuxt", "@nuxthq/studio"],
   unocss: {
     preflight: true,
   },
