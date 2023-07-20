fbc1eb4d56cf84b8363a10687035a2d3c07675a6
Jacob Long <jclong98@gmail.com>
2023-07-20 10:21:20 -0700

    refactor dark toggle

---
+++ components/DarkToggle.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <button
+    aria-label="Toggle Dark Mode"
+    :title="`Switch to ${isDark ? 'light' : 'dark'} mode`"
+    class="text-xl"
+    type="button"
+    role="switch"
+    @click="toggleDark()"
+  >
+    <i class="i-iconoir-half-moon dark:i-iconoir-sun-light" />
+  </button>
+</template>
