401f11d8e9e51f4eb54cb889d6d7f1a39dc8119a
Jacob Long <jclong98@gmail.com>
2024-01-18 12:21:53 -0700

    remove studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/content", "@unocss/nuxt", "@vueuse/nuxt", "@nuxthq/studio"],
+  modules: ["@nuxt/content", "@unocss/nuxt", "@vueuse/nuxt"],
   unocss: {
     preflight: true,
   },
